f8d823d55006cff8c9dcd7b4161555b7c6a67d67
refanr <reynirg20@ru.is>
2022-04-15 22:25:14 +0000

    complete design

--- src/index.js
+++ src/index.js
@@ -1,26 +1,13 @@
 import React from "react";
-import { ReactDOM } from "react";
+import * as ReactDOM from 'react-dom'
 import "./style.css";
+import App from "./App"
 
-function App() {
-    return (
-        <div>
-            <header>
-                <h2>ReactFacts</h2>
-                <h3>React Course - Project 1</h3>
-            </header>
-            <h1>Fun facts about React</h1>
-            <ul>
-                <li>Was first released in 2013</li>
-                <li>Was originally created by Jordan Walke</li>
-                <li>Has well over 100K stars on GitHub</li>
-            </ul>
-        </div>
-    )
-}
+
 
 
 ReactDOM.render(
     <App />,
+
     document.getElementById("root")
 )
